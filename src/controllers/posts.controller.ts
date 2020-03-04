1ef799ca5ae26716a47db1ce0777365055cded6d
Grzegorz Laszczak <g.laszczak@selleo.com>
2020-03-04 14:37:38 +0100

    Delete controllers folder

--- src/controllers/posts.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export const postsController = {
-	allPosts: async (req: Request, res: Response) => {
-		res.status(200).json({
-			data: "response"
-		});
-	}
-};
